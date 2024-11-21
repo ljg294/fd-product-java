e0bcc56dedd4aedcddab5d14906a2c325ba134cb
ljg294 <ljg29494@gmail.com>
2024-11-21 11:28:00 +0900

    api server module added

---
+++ product-api-server/src/test/java/com/fitdine/product/api/server/ProductApiServerApplicationTest.java
@@ -0,0 +1,15 @@
+package com.fitdine.product.api.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class ProductApiServerApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
