f32220709c6b5fa0667913b53b1c4db8c436bfba
ljg294 <ljg29494@gmail.com>
2024-11-24 22:11:00 +0900

    Base Entity and essential converter setup

---
+++ product-domain/src/main/java/com/fitdine/product/domain/converter/BooleanToYNConverter.java
@@ -0,0 +1,22 @@
+package com.fitdine.product.domain.converter;
+
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+
+@Converter
+public class BooleanToYNConverter implements AttributeConverter<Boolean, String > {
+
+    private static final String Y = "Y";
+    private static final String N = "N";
+
+    @Override
+    public String convertToDatabaseColumn(Boolean attribute) {
+        return (attribute != null && attribute) ? Y : N;
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String dbData) {
+        return Y.equals(dbData);
+    }
+}
+
